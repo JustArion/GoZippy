6cd2dcc0b7218a40a5c9418317072994ed939cac
arion <53127594+Arion-Kun@users.noreply.github.com>
2022-03-04 01:33:40 +0100

    Create GoZippy_test.go

---
+++ GoZippy_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var Test_Links = []string{
+	"https://www3.zippyshare.com/v/CDCi2wVT/file.html",
+}
+
+func TestSiteConnection(t *testing.T) {
+	for _, link := range Test_Links {
+		_, err1 := GetLinkContent(link)
+		if err1 != nil {
+			t.Error(fmt.Sprintf("Site is offline on link: %s", link))
+		}
+	}
+}
+
+func TestMakeFileZippyFile(t *testing.T) {
+
+	for _, link := range Test_Links {
+		sitePtr, err1 := GetLinkContent(link)
+		if err1 != nil {
+			t.Error(fmt.Sprintf("Site is offline on link: %s", link))
+		}
+
+		_, successful := TryMakeZippyFile(sitePtr)
+		if !successful {
+			t.Error(fmt.Sprintf("Failed to make zippy file for %s", link))
+		}
+	}
+
+}
